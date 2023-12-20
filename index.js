d2063db7287806c76d35309a03d6f6e13685d464
Kylie Clark <kyliedarlene@icloud.com>
2023-12-19 17:27:30 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 40;
+let num4 = 10;
 
-// Write your code here
+const multiply = num1 * num2;
+const random = Math.round(Math.random() * 10);
+const mod = num3 / num4;
+const max = Math.max(1, 3, 20);
+
+console.log(random);
